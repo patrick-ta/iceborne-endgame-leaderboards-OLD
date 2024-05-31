5acc011ca09c07368b8132e58eee5e03e5f5b0af
patrick-ta <patrick.ta451@gmail.com>
2024-05-31 13:23:04 -0400

    Added some auth logic

--- iceborne-endgame-leaderboards/src/pages/Login.jsx
+++ iceborne-endgame-leaderboards/src/pages/Login.jsx
@@ -3,10 +3,23 @@ import Header from "../components/Header";
 import "./Login.css"
 
 function Login() {
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
     const [isSignUpActive, setIsSignUpActive] = useState(false);
     const handleMethodChange = () => {
         setIsSignUpActive(!isSignUpActive);
     }
+
+    const handleSignUp = () => {
+        if (!email || !password) console.log("a");
+    }
+
+    const handleSignIn = () => {
+        if (!email || !password) console.log("a");
+    }
+
+    const handleEmailChange = (event) => setEmail(event.target.value);
+    const handlePasswordChange = (event) => setPassword(event.target.value);
 
     return (
         <>
@@ -16,11 +29,11 @@ function Login() {
             {!isSignUpActive && <h1 className="title">Login</h1>}
             
             <div className="input">
-                <input type="text" placeholder="E-mail address"/> 
-                <input type="password" placeholder="Password"/>
+                <input type="text" placeholder="E-mail address" onChange={handleEmailChange}/> 
+                <input type="password" placeholder="Password" onChange={handlePasswordChange}/>
                 <div className="input-flex">
-                    {isSignUpActive && <button type="button">Sign Up</button>}
-                    {!isSignUpActive && <button type="button">Sign In</button>}
+                    {isSignUpActive && <button type="button" onClick={handleSignUp}>Sign Up</button>}
+                    {!isSignUpActive && <button type="button" onClick={handleSignIn}>Sign In</button>}
                     
                     {isSignUpActive && <a onClick={handleMethodChange}>Already have an account?</a>}
                     {!isSignUpActive && <a onClick={handleMethodChange}>Create an account</a>}
