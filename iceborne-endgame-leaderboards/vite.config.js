d98bddec776b48754c409974b7803ccfa5ba5afd
patrick-ta <patrick.ta451@gmail.com>
2024-05-31 21:12:12 -0400

    updated deploy config

--- iceborne-endgame-leaderboards/vite.config.js
+++ iceborne-endgame-leaderboards/vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/iceborne-endgame-leaderboards/",
+  base: "/mhwib-leaderboards/",
 })
